54a9ec1691ad49521e62f046455f2964ef03fcf4
mohyangba <mohyangba@users.noreply.github.com>
2023-11-16 16:04:11 +0900

    04 retry

--- main.c
+++ main.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap (int *x, int *y) {
-     int temp;
-     temp = *x;
-     *x = *y;
-     *y = temp;
-     }
-     
 int main(int argc, char *argv[])
 {
   int i;
   int grade[5];
-  int average=0;
+  int sum=0; // 합
   
+  //grade에 입력 
   for (i=0;i<5;i++) 
   {
-      printf("grade[%i] = ", i);
+      printf("Entergrade[%i] = \n", i);
       scanf("%d", &grade[i]);
   }
   
   //반복문 및 포인터 문법으로 grade 배열값 출력  
   for (i=0;i<5;i++) 
   {
-      printf("grade[%i] = %i\n", *(grade+i));
-      // 평균 값 계산  
-      average = average + *(grade+i);
+      printf("grade[%i] = %i\n", i, grade[i]);
+     sum += *(grade+i);
   }
-  
+   // 평균 값 계산  
+  float average = (float) sum/5;
+  printf("average = %.2f\n", average);
+   
   system("PAUSE");	
   return 0;
 }
