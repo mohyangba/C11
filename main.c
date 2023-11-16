c9232ed8920422faebbc8dda5fc53c6c4d2a2935
mohyangba <mohyangba@users.noreply.github.com>
2023-11-16 15:39:22 +0900

    03 call by value

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int x, int y) {
+     int temp;
+     temp = x;
+     x=y;
+     y=temp;
+     }
+     
+int main(int argc, char *argv[])
+{
+  int a = 3;
+  int b = 5;
+  
+  swap(a,b);
+  
+  printf("a: %i, b: %i \n", a, b);
+  
+  system("PAUSE");	
+  return 0;
+}
