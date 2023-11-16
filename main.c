bbd77655a76b257464f1f80671dac32b789c53fb
mohyangba <mohyangba@users.noreply.github.com>
2023-11-16 16:11:16 +0900

    01

--- main.c
+++ main.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  char *pc;
-  int *pi;
-  double *pd;
-  
-  pc = (char *)10000;
-  pi = (int*)10000;
-  pd = (double*)10000;
-  
-  printf("증가 전 : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-  
-  pc++;
-  pi++;
-  pd++;
-  
-  printf("증가 후: pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
-  system("PAUSE");	
-  return 0;
+int main(int argc, char *argv[]) {
+    int i = 4;
+
+    int *pi = &i;
+    char *pc = (char *)&i; // Cast the pointer to char
+
+    printf("%i, %i, %i\n", i, *pi, *pc); // Corrected pointer dereference
+
+    system("PAUSE");
+    return 0;
 }
